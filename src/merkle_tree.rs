83b03b78e8afb97843ac6724a38c7900302765ec
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-20 13:08:32 -0300

    Implement Leaf::new()

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,5 +1,7 @@
 use sha3::{Keccak256, Digest};
 
+type Position = usize;
+
 struct Leaf {
     // The hash is an array of 32 u8
     hash: [u8; 32],
@@ -7,9 +9,27 @@ struct Leaf {
     // Each leaf will have an index to its children.
     // It's an option type because maybe the child does not
     // exist.
-    left_child: Option<usize>,
-    right_child: Option<usize>
+    left_child: Option<Position>,
+    right_child: Option<Position>,
+
+    // // NOTE: I am NOT a fan of a leaf having it's own index. It seems
+    // // like duplicating information, which can lead to false assumptions.
+    // // I hope it doesnt bite me in the butt later on.
+    // my_position: Position,
+
 }
+
+impl Leaf {
+    pub fn new(hash: [u8; 32], left_child: Option<Position>, right_child: Option<Position>) -> Leaf {
+        Leaf {
+	  hash,
+	  left_child,
+	  right_child,
+        }
+    }
+
+}
+
 
 struct MerkleTree {
     leaves: Vec<Leaf>,
