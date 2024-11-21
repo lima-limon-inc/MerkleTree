7d3790ec79f9ee9996686202dc044b4345fe3758
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-11-21 17:38:17 -0300

    Finish validation creation.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod merkle_tree;
 fn main() {
     println!("Hello, world!");
     let merkle_tree = merkle_tree::MerkleTree::new(&[
-					  "0",
 					  "1",
 					  "2",
 					  "3",
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,6 +1,5 @@
 use sha3::{Digest, Sha3_256};
 
-type Position = usize;
 type HashedData = [u8; 32];
 
 pub fn hash(leaves: &[HashedData]) -> HashedData {
@@ -54,9 +53,68 @@ impl MerkleTree {
 	  }
         }
 
-
         MerkleTree { leaves: tree }
 
+    }
+
+    pub fn generate_proof<T: std::convert::AsRef<[u8]>>(&self, elem: &T) -> Option<Vec<HashedData>> {
+
+        // It there is no first level, for whatever reason, reaturn None
+        let first_level = self.leaves.get(0)?;
+
+        // If the requested item is not present in the data block, we
+        // also return None
+        let mut index = first_level
+	  .iter()
+	  .position(|og_data| {
+	      let check: HashedData = Sha3_256::digest(elem).into();
+	      *og_data == check
+	  })?;
+
+        // We need the brother of the requested item.
+        let brother = if index % 2 == 0 { index + 1 } else { index - 1 };
+        let mut needed_values: Vec<_> = vec![index, brother];
+
+        // The first two values *MUST* be sorted, since the hashing
+        // function varies output depending on order.
+        needed_values.sort();
+
+        // NOTE: This section is very imperative, I'd like to simplify
+        // it later, for now it works. But I am not a fan at all.
+        // This is just a WIP
+        for _ in self.leaves.iter().skip(1) {
+	  let parent_index = (index as f32 / 2.0).floor() as usize;
+	  let uncle_index = if parent_index % 2 == 0 { parent_index + 1 } else { parent_index - 1 };
+
+	  index = parent_index;
+	  needed_values.push(uncle_index);
+        }
+
+        // The last element contains the needed value for the root,
+        // which we don't need. Thus, we remove it
+        needed_values.remove(needed_values.len() - 1);
+        println!("{:?}", needed_values);
+
+        // NOTE: There is no reason for me to go through the vector
+        // twice. I could store all the values in one go. I am doing
+        // this to simply debugging. 
+        let first_value = self.leaves[0][needed_values[0]];
+        let mut hashes: Vec<HashedData> = vec![first_value];
+
+        let mut required_index = needed_values.iter().skip(1);
+
+        for level in &self.leaves {
+	  if level.len() == 1 {
+	      break;
+	  }
+	  if let Some(index) = required_index.next() {
+	      let value = level[*index];
+	      hashes.push(value);
+	  } else {
+	      panic!("INVARIANT HAS BEEN BROKEN. THIS SHOULD NOT HAPPEN")
+	  }
+        }
+        Some(hashes)
     }
 }
 
@@ -116,4 +174,23 @@ mod tests {
         let merkle_tree = MerkleTree::new(&["0", "1", "2", "3"]);
         assert_eq!(merkle_tree.leaves.len(), 3)
     }
+
+    #[test]
+    fn generate_proof_test() {
+
+        let merkle_tree = MerkleTree::new(&[
+	  "0",
+	  "1",
+	  "2",
+	  "3",
+	  "4", 
+	  "5", 
+	  "6", 
+	  "7", 
+        ]);
+
+        merkle_tree.generate_proof(&"5");
+        assert!(false)
+    }
+
 }
